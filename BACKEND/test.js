11e07b32903028192ed788dec5d71c6156f99109
Princi <princikirme12345@gmail.com>
2026-07-03 18:15:34 +0530

    test.js

--- BACKEND/test.js
+++
@@ -1,9 +0,0 @@
-
-
-const targetDate = new Date();
-targetDate.setMonth(targetDate.getMonth() - 6);
-
-console.log(targetDate);
-
-console.log(targetDate.getFullYear());
-console.log(targetDate.getMonth()+1)
