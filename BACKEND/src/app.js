24741ba437b0085a51f935912fa5fd60365c0ced
Princi <princikirme12345@gmail.com>
2026-07-30 19:25:38 +0530

    Added render url in cors origin path1.

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -19,7 +19,7 @@ mongooseConnect();
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ["https://welwiser-1.onrender.com", "http://localhost:5174"];
+const allowedOrigins = ["https://welwiser.onrender.com", "http://localhost:5174"];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.includes(origin)) {
